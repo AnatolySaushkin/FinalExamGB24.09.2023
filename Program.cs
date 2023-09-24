c5913c9ec2fd3da1e92c8e3d295664d664ffdbf9
Walther0296 <sau100@inbox.ru>
2023-09-24 21:45:40 +0300

    Выполнение Задания. Программа составлена

--- Program.cs
+++ Program.cs
@@ -1,2 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string Prompt(string message)
+{
+    System.Console.Write(message);
+    string result = System.Console.ReadLine()!;
+    return result;
+}
+
+System.Console.Write("Введите количество элементов массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int M = 0;
+string[] Array1 = GenerateArray(N);
+Console.Write("Результат выполнения задачи: ");
+PrintArray(Array1);
+CountNewArray(N);
+string[] Array2 = GenerateNewArray(M);
+PrintNewArray(Array2);
+
+// Задаем элементы массива
+string[] GenerateArray(int N)
+{
+    string[] Array1 = new string[N];
+    for (int i = 0; i < N; i++)
+    {
+        Array1[i] = Prompt($"Введите Элемент массива № {i + 1}: ");
+    }
+    return Array1;
+}
+void PrintArray(string[] Array1)
+{
+    for (int i = 0; i < N; i++)
+    {
+        System.Console.Write(Array1[i] + " ");
+    }
+    System.Console.Write(" -> ");
+}
+
+int CountNewArray(int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        if (Array1[i].Length <= 3)
+        {
+            M++;
+        }
+    }
+    return M;
+}
+
+string[] GenerateNewArray(int M)
+{
+    string[] Array2 = new string[M];
+    int a = 0;
+    for (int i = 0; i < N; i++)
+    {
+        if (Array1[i].Length <= 3)
+        {
+            Array2[a] = Array1[i];
+            a++; 
+        }
+    }
+    return Array2;
+}
+
+void PrintNewArray(string[] Array2)
+{
+    for (int i = 0; i < M; i++)
+    {
+        System.Console.Write(Array2[i] + " ");
+    }
+}
